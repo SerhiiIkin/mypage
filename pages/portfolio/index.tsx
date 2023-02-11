a31f228c7a50ff3fd2f2692cb66a6fb4a379a1e4
SerhiiIkin <messileonl@gmail.com>
2023-02-11 14:42:59 +0100

    img change

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -21,7 +21,7 @@ function Portfolio() {
                 <h1 className="text-center text-xl lg:text-4xl pb-2">
                     {projectsTitle}
                 </h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4  xl:gap-x-8 ">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 xl:gap-x-8">
                     {projects()
                         .reverse()
                         .map((project) => {
